1708eed50d55ac541ca115e4c0f3925f08ba2798
Eric Foy <foy.8@wright.edu>
2019-02-21 22:20:32 -0500

    moved src files to /server/src

--- server/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    host: 'sql.tabletopartistry.net',
-    port: '8100',
-    user: 'tunnelapp-user',
-    password: 'Cs3900',
-    database: 'wsutunnelapp'
-};
-
-module.exports = config;
